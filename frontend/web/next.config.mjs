66d95e5ccebdcfe54fe375f9e56a4aa1eb5c3329
s-duu-jp <s@duu.jp>
2024-08-02 19:53:17 +0900

    ビルドオプション修正 - styleが適用されない問題を解消

--- frontend/web/next.config.mjs
+++ frontend/web/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'out'
+    distDir: 'out',
+    assetPrefix: './',
+    trailingSlash: true,
   };
 
 export default nextConfig;
